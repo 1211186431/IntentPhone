568e4c7da80887acbf1155d23dfdf702a8e45cd6
1211186431 <56494120+1211186431@users.noreply.github.com>
2020-09-12 09:46:48 +0800

    intent隐式启动

---
+++ app/src/main/java/com/example/intentphone/callphone.java
@@ -0,0 +1,14 @@
+package com.example.intentphone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class callphone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_callphone);
+    }
+}
